598cf592b5855310e357d4db65a303dd63f751d0
sudane@gmail.com <sudane@gmail.com>
2019-01-22 11:30:53 +0000

    Adding All Files

---
+++ build/precache-manifest.b7bf203f40add337ec78bbceecc4a662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47861bd8df727b5fa207",
+    "url": "/static/css/main.bb9029f0.chunk.css"
+  },
+  {
+    "revision": "47861bd8df727b5fa207",
+    "url": "/static/js/main.47861bd8.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "baf7bd869ed4aa8b359d62ebd6ae6dcb",
+    "url": "/index.html"
+  }
+];
